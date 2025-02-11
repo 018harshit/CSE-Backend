91cec59bda9f73e2ca1ef0497f129bb77cfbf926
Harshit <harshitchaudhary2608@gmail>
2025-02-11 10:13:44 +0530

    Updated again

--- CSE-c/FileSystem/filereadsync.js
+++ CSE-c/FileSystem/filereadsync.js
@@ -1,3 +1,18 @@
 const fs = require("fs"); 
-const data = fs.readFileSync("./data.txt");
+
+// fs.writeFileSync("./data1.txt", "Hello Abes","utf-8");
+// fs.writeFileSync("./data1.txt", "Hello ABESEC","utf-8");
+// fs.appendFileSync("./data1.txt", "Student","utf-8");
+
+// fs.renameSync("./data2.txt","./data3.txt");
+// fs.unlinkSync("./data3.txt");
+// const data = fs.readFileSync("./data.txt","utf-8");
+// // console.log(data.toString());
+// console.log(data)
+const data = fs.readFileSync("./data.txt","utf-8");
 console.log(data)
+if(data.match("H")){
+    console.log("File contains 'H'");
+    const newData = data.replace("H","Abes");
+    fs.writeFileSync("./data.txt",newData,"utf-8");
+}
