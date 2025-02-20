cebfd7e3ee5b2cdb44ac4342d4caf99bda0e5379
Harshit <harshitchaudhary2608@gmail>
2025-02-20 15:32:38 +0530

    workdone

---
+++ CSE-c/HTTP/http-plain.js
@@ -0,0 +1,15 @@
+// const { promises } = require('dns');
+const http = require('http');
+const fs =require('fs/promises');
+const port = 3000;
+const server = http.createServer(async(req,res)=>{
+    // res.statusCode=200;
+    // res.setHeader('Content-Type','text/plain');
+    res.writeHead(200,{ 'Content-Type': 'text/html' });
+    const readdata = await fs.readFile("./home.html",'utf8');
+    res.end(readdata);
+});
+
+server.listen(port,()=>{
+    console.log(`Server is running on the port ${port}`);
+});
